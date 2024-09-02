ffc9e813ead9f8579fce7b586c52da2838395dbf
Hami Hawkins <hhaami@gmail.com>
2024-09-02 14:57:51 +1200

    Client side delete test working

--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -8,19 +8,17 @@ const router = express.Router()
 // GET /api/v1/locations
 router.get('/', async (req, res, next) => {
   try {
-    // TODO: Replace this with all of the locations in the database
     const locations = await getAllLocations()
-
     res.json({ locations })
   } catch (e) {
     next(e)
   }
 })
 
+// GET /api/v1/locations/:id
 router.get('/:id', async (req, res, next) => {
   const id = Number(req.params.id)
   try {
-    // TODO: Get the location based on its id and replace this viewData
     const location = await getLocationById(id)
     res.json(location)
   } catch (e) {
@@ -28,11 +26,11 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
+// PATCH /api/v1/locations/:id
 router.patch('/:id', async (req, res, next) => {
   try {
     const id = Number(req.params.id)
     const { name, description } = req.body
-    // TODO: call db.updateLocation with these details
     await db.updateLocation({ id, name, description })
     res.sendStatus(204)
   } catch (e) {
--- server/routes/schedule.ts
+++ server/routes/schedule.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { getEventsByDay } from '../db/index.ts'
 import { validateDay } from './helpers.ts'
-import * as db from '../db/index.ts'
 
 const router = express.Router()
 
@@ -9,7 +8,6 @@ const router = express.Router()
 router.get('/:day', async (req, res, next) => {
   try {
     const day = validateDay(req.params.day)
-    // TODO: replace this data with the results of calling db.getEventsByDay
     const events = await getEventsByDay(day)
     res.json({ day, events })
   } catch (e) {
